f24030e7e4dcdb55a77a0f6a4ad6ddb955934a22
Francis Yeong <francisyeong96@gmail.com>
2021-11-22 10:11:10 +0800

    Replace arbitrary color values with color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   mode: "jit",
-  purge: ["./*.html"],
+  purge: ["./*.html", "./src/*.css"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      scale: {
-        80: ".8",
-        85: ".85",
+      colors: {
+        // Colour pallette used in this project
+        // Names from color-name.com
+        "alabaster": "#f4f1e3",
+        "beaver": "#908669",
+        "bone": "#dfd9c9",
+        "champagne": "#f4ecca",
+        "dark-bronze-coin": "#4d3906",
+        "dutch-white": "#ede1b8",
+        "pearl-aqua": "#82d7aa",
       },
     },
   },
